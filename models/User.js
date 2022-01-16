f081d251ef5442bc73c6171c6847184c39e98ed6
nampt <nampt@Phams-MacBook-Pro.local>
2022-01-16 15:26:34 +0700

    feat: 🎸 setup register and encrypt password

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true},
-  email: { type: String, required: true, unique: true},
-  password: { type: String, required: true, minlength: 5, maxlength: 8}
+  email: { type: String, required: true},
+  password: { type: String, required: true}
 })
 
 const User = mongoose.model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
+const bcrypt = require('bcrypt');
 var router = express.Router();
+const UserModel = require('../models/User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body; // destructuring ES6
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const data = {
+      name,
+      email,
+      password: hashedPassword
+    }
+  
+    const user = await UserModel.create(data);
+    if(user) return res.redirect('/login');
+  } catch (error) {
+    console.log("🚀 ~ file: index.js ~ line 24 ~ router.post ~ error", error)
+    next(error)
+  }
+})
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
 
 module.exports = router;
