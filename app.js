e5250d5181455b9726d1b60a732972d32e5dd408
nampt <nampt@Phams-MacBook-Pro.local>
2022-02-22 20:55:44 +0700

    feat: 🎸 send request to view all users

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
@@ -24,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
