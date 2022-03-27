6a4db6d831345c5640734750164506cec9b9abb8
nampt <nampt@Phams-MacBook-Pro.local>
2022-03-27 15:34:06 +0700

    feat: 🎸 update user

---
+++ FE/javascripts/UserServices.js
@@ -0,0 +1,69 @@
+const getUsers = async () => {
+  try {
+    const URI = `http://localhost:3000/api/users`;
+
+    const response = await axios.get(URI);
+
+    return response.data;
+
+  } catch (error) {
+    console.log("🚀 ~ file: index.html ~ line 63 ~ getUsers ~ error", error)
+    return [];
+  }
+}
+
+const getUser = async (userId) => {
+  try {
+    const URI = `http://localhost:3000/api/user/${userId}`;
+
+    const response = await axios.get(URI);
+
+    return response.data;
+
+  } catch (error) {
+    console.log("🚀 ~ file: index.html ~ line 63 ~ getUsers ~ error", error)
+    return [];
+  }
+}
+
+const deleteUser = async (userId) => {
+  try {
+    const URI = `http://localhost:3000/api/user/${userId}`;
+
+    const response = await axios.delete(URI);
+
+    return response.data;
+
+  } catch (error) {
+    console.log("🚀 ~ file: index.html ~ line 63 ~ deleteUsers ~ error", error)
+    return [];
+  }
+}
+
+const createUser = async (data) => {
+  try {
+    const URI = `http://localhost:3000/api/user`;
+
+    const response = await axios.post(URI, data);
+
+    return response.data;
+
+  } catch (error) {
+    console.log("🚀 ~ file: index.html ~ line 63 ~ deleteUsers ~ error", error)
+    return [];
+  }
+}
+
+const updateUser = async (userId, data) => {
+  try {
+    const URI = `http://localhost:3000/api/user/${userId}`;
+
+    const response = await axios.put(URI, data);
+
+    return response.data;
+
+  } catch (error) {
+    console.log("🚀 ~ file: index.html ~ line 63 ~ updateUser ~ error", error)
+    return [];
+  }
+}
